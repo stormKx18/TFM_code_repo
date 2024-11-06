11a5130ae1b9be3d9291d591069c34a11b4adca5
hirian <vicjaime922@gmail.com>
2024-11-05 21:48:20 -0600

    Change to database fields

--- IoT_device/raspberry_pubIOT.py
+++ IoT_device/raspberry_pubIOT.py
@@ -14,6 +14,7 @@ import nmap
 from picamera import PiCamera  # Assuming you use Raspberry Pi Camera for capturing images
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # MQTT Configuration
 ENDPOINT = "a3mzq67wa3z4ym-ats.iot.us-east-1.amazonaws.com"
@@ -150,35 +151,40 @@ def upload_to_s3(image_path, bucket_name, s3_key):
     except NoCredentialsError:
         print("Credentials not available.")
 
-def get_external_temperature_humidity():
-    url = "https://www.meteored.mx/ciudad-satelite-1-524023.html"
+def get_ambient_conditions():
+    METRICS=4
+    url = "https://www.meteored.mx/ciudad-de-mexico/historico"
     try:
         response = requests.get(url, timeout=10)  # Agrega un timeout en caso de que la solicitud tarde demasiado
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
             
             # Selecciona los elementos de temperatura y humedad
-            temperature_element = soup.select_one("#d_hub_1 > span > span.row.info-act > span.temperatura > span > span > span.dato-temperatura.changeUnitT")
-            humidity_element = soup.select_one("#d_hub_1 > span > span.row.info-act > span.datos-uv.col > span:nth-child(2) > strong")
-
+            temperature_element = soup.select_one("#ult_dato_temp")
+            humidity_element = soup.select_one("#ult_dato_hum")
+            wind_speed_element = soup.select_one("#ult_dato_velviento")
+            wind_direction_element_raw = soup.select_one("#ult_dato_dirviento")
+            wind_direction_element= re.search(r"\((\d+)", wind_direction_element_raw.get_text()).group(1)
             # Verifica que ambos elementos existen antes de intentar extraer los datos
             if temperature_element and humidity_element:
-                temperature = float(temperature_element.get_text().strip().rstrip('°'))
-                humidity = float(humidity_element.get_text().strip().rstrip('%'))
-                return [temperature, humidity]
+                temperature = float(temperature_element.get_text())
+                humidity = float(humidity_element.get_text())
+                wind_speed=float(wind_speed_element.get_text())
+                wind_direction=float(wind_direction_element)
+                return [temperature, humidity, wind_speed, wind_direction]
             else:
                 print("No se encontraron los elementos de temperatura o humedad.")
-                return [-100, -100]
+                return [-100]*METRICS
         else:
             print(f"Error al acceder a la página. Código de estado: {response.status_code}")
-            return [-100, -100]
+            return [-100]*METRICS
     except requests.exceptions.RequestException as e:
         print(f"Error de red al obtener la temperatura y humedad: {e}")
-        return [-100, -100]
+        return [-100]*METRICS
     except ValueError as e:
         print(f"Error al convertir los datos a números: {e}")
-        return [-100, -100]
-
+        return [-100]*METRICS
+    
 if __name__ == '__main__':
     # Create a MQTT connection
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
@@ -219,7 +225,7 @@ if __name__ == '__main__':
         network_info = get_network_info()
         # Get the number of hosts
         number_of_hosts = get_hosts_count()
-        external_temp, external_humidity = get_external_temperature_humidity()
+        ext_temp, ext_humidity, wind_spd, wind_dir = get_ambient_conditions()
 
         # Create message payload
         message_json = json.dumps(
@@ -235,8 +241,10 @@ if __name__ == '__main__':
                 "download_speed": f"{network_info['download_speed']:.3f}",
                 "upload_speed": f"{network_info['upload_speed']:.3f}",
                 "number_of_hosts": f"{number_of_hosts}",
-                "external_temperature": f"{external_temp}",
-                "external_humidity": f"{external_humidity}"
+                "temperature": f"{ext_temp}",
+                "humidity": f"{ext_humidity}",
+                "wind_speed": f"{wind_spd}",
+                "wind_direction": f"{wind_dir}",
             }, indent=2
         )
 
